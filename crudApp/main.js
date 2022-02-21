cb98ded1af9ca37c794410ec451547c14047fc8a
dhiraj Jayswal <dhirajjayswal0@gmail.com>
2022-02-21 23:07:29 +0530

    Project Completion
    Project completed

---
+++ crudApp/main.js
@@ -0,0 +1,42 @@
+// imports
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session')
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//databse collection details
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', (error) => console.Consolelog(error));
+db.once('open', () => console.log('connected to the database!'));
+
+// milddlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(session({
+    secret: 'my secret key',
+    saveUninitialized: true,
+    resave: false
+}));
+
+app.use((req, res, next) => {
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+});
+
+app.use(express.static("uploads"));
+
+// set template engine
+app.set('view engine', 'ejs');
+
+// route prefix
+app.use("", require('./routes/routes'));
+
+app.listen(PORT, () => {
+    console.log(`server started at http://localhost:${PORT}`);
+})
